95c944c139a6ff24ea39750f98b2b28902af93a7
Wazabii <daniel@creativearmy.se>
2023-12-06 16:39:18 +0100

    Autoload adjustment

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 use MaplePHP\Foundation\Kernel\Kernel;
 $dir = realpath(dirname(__FILE__) . '/..') . '/';
-require_once("{$dir}app/Libraries/Foundation/autoload.php");
+if(is_file("{$dir}app/Libraries/Foundation/autoload.php")) {
+	require_once("{$dir}app/Libraries/Foundation/autoload.php");
+}
+if(is_file("{$dir}vendor/autoload.php")) {
+	require_once("{$dir}vendor/autoload.php");
+}
 $kernel = new Kernel($dir);
 $kernel->run();
