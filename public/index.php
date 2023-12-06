51b8aa8c1a03ada0aa7b99c3659a34dd7e7862ae
Wazabii <daniel@creativearmy.se>
2023-12-06 17:05:38 +0100

    Loader patch

--- public/index.php
+++ public/index.php
@@ -1,14 +1,9 @@
 <?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 use MaplePHP\Foundation\Kernel\Kernel;
 $dir = realpath(dirname(__FILE__) . '/..') . '/';
-if(is_file("{$dir}app/Libraries/Foundation/autoload.php")) {
-	require_once("{$dir}app/Libraries/Foundation/autoload.php");
-}
-if(is_file("{$dir}vendor/autoload.php")) {
-	require_once("{$dir}vendor/autoload.php");
-}
+// This will change 
+require_once("{$dir}vendor/maplephp/foundation/autoload.php");
+require_once("{$dir}vendor/autoload.php");
+
 $kernel = new Kernel($dir);
 $kernel->run();
