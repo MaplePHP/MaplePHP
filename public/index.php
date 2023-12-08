71921a53882ede4805ee712d266ca25517cc7457
Wazabii <daniel@creativearmy.se>
2023-12-08 13:17:32 +0100

    Minor changes

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php 
 use MaplePHP\Foundation\Kernel\Kernel;
 $dir = realpath(dirname(__FILE__) . '/..') . '/';
-// This will change 
-require_once("{$dir}vendor/maplephp/foundation/autoload.php");
-require_once("{$dir}vendor/autoload.php");
-
+// This will change (will tho only affect commits)
+$autoload = "{$dir}app/Libraries/Foundation/autoload.php";
+if (is_file("{$dir}vendor/maplephp/foundation/autoload.php")) {
+	$autoload = "{$dir}vendor/maplephp/foundation/autoload.php";
+}
+require_once($autoload);
 $kernel = new Kernel($dir);
 $kernel->run();
