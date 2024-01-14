92b3cec7be60065051a284aafc505870517076cf
Wazabii <daniel@creativearmy.se>
2024-01-14 15:20:53 +0100

    Examples

---
+++ node_modules/stratoxcomponents/src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Stratox components index file
+ * Author: Daniel Ronkainen
+ * Description: A modern JavaScript template library that redefines how developers can effortlessly create dynamic views.
+ * Copyright: Apache License 2.0
+ */
+
+import { StratoxForm } from './StratoxForm.js';
+import { StratoxIngress } from './StratoxIngress.js';
+import { StratoxModal } from './StratoxModal.js';
+import { StratoxTable } from './StratoxTable.js';
+
+export { StratoxForm, StratoxIngress, StratoxModal, StratoxTable };
